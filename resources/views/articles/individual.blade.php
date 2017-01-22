f5cdcb28790f46cee2ff3d5d423d0f14b0f17fce
Jyrone Parker <jyrone.parker@gmail.com>
2017-01-22 17:09:53 -0500

    google ads

--- resources/views/articles/individual.blade.php
+++ resources/views/articles/individual.blade.php
@@ -8,7 +8,27 @@
                 <div class="panel-heading">{{$article->title}}</div>
 
                 <div class="panel-body">
+                  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                  <!-- all-around-politics -->
+                  <ins class="adsbygoogle"
+                       style="display:block"
+                       data-ad-client="ca-pub-7023023584987784"
+                       data-ad-slot="2351142156"
+                       data-ad-format="auto"></ins>
+                  <script>
+                  (adsbygoogle = window.adsbygoogle || []).push({});
+                  </script>
                     {!!$article->description!!}
+                    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                    <!-- all-around-politics -->
+                    <ins class="adsbygoogle"
+                         style="display:block"
+                         data-ad-client="ca-pub-7023023584987784"
+                         data-ad-slot="2351142156"
+                         data-ad-format="auto"></ins>
+                    <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>
                     <br>
                     Original Link <a href="{{$article->link}}">here</a>
                 </div>
