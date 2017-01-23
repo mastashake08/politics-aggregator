70d6817046ca95e903c91aecb407b6e2a0d6c888
Jyrone Parker <jyrone.parker@gmail.com>
2017-01-23 14:52:38 -0500

    distinct

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -14,7 +14,7 @@ class ArticleController extends Controller
     public function index()
     {
         //
-        $articles = Article::orderBy('created_at','desc')->paginate(10);
+        $articles = Article::orderBy('created_at','desc')->distinct()->paginate(10);
         $with = [
           'articles' => $articles
         ];
