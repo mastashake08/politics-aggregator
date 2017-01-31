7b87219d6a0c9b4a1ef880d24f03455fcca53a37
Jyrone Parker <jyrone.parker@gmail.com>
2017-01-31 18:04:50 -0500

    api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::get('/articles',function(){
+  return \App\Article::paginate(25);
+});
