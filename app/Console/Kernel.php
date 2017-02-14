15de6c63901bdf6c9969d947194d4d769e280e45
Jyrone Parker <jyrone.parker@gmail.com>
2017-02-14 11:20:51 -0500

    typo

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -45,7 +45,7 @@ class Kernel extends ConsoleKernel
 
              $article = \App\Article::where('title' , $link->get_title())->first();
              if($article == null){
-               if($link->get_description != null){
+               if($link->get_description() != null){
              $article = \App\Article::Create(['title' =>$link->get_title(),
               'description' => $link->get_description(),
               'link' => $link->get_link(),
