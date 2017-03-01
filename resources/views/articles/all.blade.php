4134011b4d7778a1652684047fdbb1979b27e940
Jyrone Parker <jyrone.parker@gmail.com>
2017-02-28 23:18:54 -0500

    link

--- resources/views/articles/all.blade.php
+++ resources/views/articles/all.blade.php
@@ -30,7 +30,7 @@
                         <td>
                           <h2><a href="/articles/{{$article->url}}" target="_blank">{{$article->title}}</a></h2>
                           <br>
-                          <h3><a href="{{$article->url}}" target="blank"></a></h3>
+                          <h3><a href="{{$article->url}}" target="blank">{{$article->url}}</a></h3>
                         </td>
 
                       </tr>
