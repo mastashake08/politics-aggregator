b644ccc54fa3027c6660049250b4ad7bc7e5738a
Jyrone Parker <mastashake08@users.noreply.github.com>
2017-03-12 10:37:51 -0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+Route::get('/users', function () {
+    return \App\User::All();
+});
 
 Route::resource('/articles','ApiArticleController');
