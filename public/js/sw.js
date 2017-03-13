f1ca6a56e1be1b15e152fbab16bb4cb30de0f926
Jyrone Parker <jyrone.parker@gmail.com>
2017-03-13 14:41:27 -0400

    permission

--- public/js/sw.js
+++ public/js/sw.js
@@ -1,12 +1,9 @@
 importScripts('socket.io-1.4.5.js');
 function notifyMe(message,link) {
-  // Let's check if the browser supports notifications
-  if (!("Notification" in window)) {
-    alert("This browser does not support desktop notification");
-  }
+
 
   // Let's check whether notification permissions have already been granted
-  else if (Notification.permission === "granted") {
+ if (Notification.permission === "granted") {
     // If it's okay let's create a notification
     if (permission === "granted") {
       var options = {
