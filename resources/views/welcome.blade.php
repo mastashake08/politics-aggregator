e2e402f75240c06acfb2d8148d56820af870ab4a
Jyrone Parker <jyrone.parker@gmail.com>
2017-03-13 19:41:51 -0400

    worker

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,7 +69,7 @@
         </style>
         <script>
         window.addEventListener('load', function() {
-         navigator.serviceWorker.register('/js/sw.js').then(function(registration) {
+         navigator.Worker.register('/js/sw.js').then(function(registration) {
            // Registration was successful
            console.log('ServiceWorker registration successful with scope: ', registration.scope);
          }).catch(function(err) {
@@ -79,7 +79,7 @@
        });
         </script>
 
-        
+
 
     </head>
     <body>
