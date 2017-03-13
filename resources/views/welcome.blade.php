7e6c1a78b9800db3294123eb50f965d684c1caac
Jyrone Parker <jyrone.parker@gmail.com>
2017-03-13 14:36:36 -0400

    sw

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,80 +67,19 @@
                 margin-bottom: 30px;
             }
         </style>
-        <script src="https://cdn.socket.io/socket.io-1.4.5.js"></script>
         <script>
-
-        function notifyMe(message,link) {
-          // Let's check if the browser supports notifications
-          if (!("Notification" in window)) {
-            alert("This browser does not support desktop notification");
-          }
-
-          // Let's check whether notification permissions have already been granted
-          else if (Notification.permission === "granted") {
-            // If it's okay let's create a notification
-            if (permission === "granted") {
-              var options = {
-                data: {
-                  url:link,
-                  sound: "{{url('/censor.mp3')}}",
-                  vibrate: [200, 100, 200]
-                }
-              };
-              var notification = new Notification(message,options);
-              notification.onclick = function(event) {
-              event.preventDefault(); // prevent the browser from focusing the Notification's tab
-              window.open("https://politics.socketdroid.com/articles/"+link, '_blank');
-            }
-            }
-          }
-
-          // Otherwise, we need to ask the user for permission
-          else if (Notification.permission !== 'denied') {
-            Notification.requestPermission(function (permission) {
-              // If the user accepts, let's create a notification
-              if (permission === "granted") {
-                var options = {
-                  data: {
-                    url:link,
-                    sound: "{{url('/censor.mp3')}}",
-                    vibrate: [200, 100, 200]
-                  }
-                };
-                var notification = new Notification(message,options);
-                notification.onclick = function(event) {
-                event.preventDefault(); // prevent the browser from focusing the Notification's tab
-                window.open("https://politics.socketdroid.com/articles/"+link, '_blank');
-              }
-              }
-            });
-          }
-
-          // At last, if the user has denied notifications, and you
-          // want to be respectful there is no need to bother them any more.
-          }
-          // Let's check if the browser supports notifications
-          if (!("Notification" in window)) {
-            alert("This browser does not support desktop notification");
-          }
-
-          // Let's check whether notification permissions have already been granted
-          else if (Notification.permission === "granted") {
-            // If it's okay let's create a notification
-
-          }
-
-          // Otherwise, we need to ask the user for permission
-          else if (Notification.permission !== 'denied') {
-            Notification.requestPermission(function (permission) {
-
-            });
-          }
-          var socket = io.connect("https://politics.socketdroid.com:6002");
-          socket.on('new-article',function(data){
-            notifyMe(data.title,data.url);
-          });
+        window.addEventListener('load', function() {
+         navigator.serviceWorker.register('/js/sw.js').then(function(registration) {
+           // Registration was successful
+           console.log('ServiceWorker registration successful with scope: ', registration.scope);
+         }).catch(function(err) {
+           // registration failed :(
+           console.log('ServiceWorker registration failed: ', err);
+         });
+       });
         </script>
+
+        
 
     </head>
     <body>
