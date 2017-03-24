ac529a7a38ab9a3133d6f47c3443b58dc8532fda
Jyrone Parker <jyrone.parker@gmail.com>
2017-03-24 17:13:39 -0400

    sandbox width

--- resources/views/articles/individual.blade.php
+++ resources/views/articles/individual.blade.php
@@ -21,7 +21,7 @@
                   <script>
                   (adsbygoogle = window.adsbygoogle || []).push({});
                   </script>
-                    <iframe src="{{$article->link}}"></iframe>
+                    <iframe src="{{$article->link}}" sandbox width="100%"></iframe>
                     <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                     <!-- all-around-politics -->
                     <ins class="adsbygoogle"
