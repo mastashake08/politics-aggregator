6ed21537bcf7a4ebadeaa882f56a9af4243fdc94
Jyrone Parker <jyrone.parker@gmail.com>
2019-03-05 23:39:50 -0500

    console

--- public/js/sw.js
+++ public/js/sw.js
@@ -27,6 +27,7 @@ self.addEventListener('activate',  event => {
   event.waitUntil(self.clients.claim());
 });
 self.addEventListener('fetch', event => {
+  console.log("Fetch",event)
   event.respondWith(
     caches.match(event.request, {ignoreSearch:true}).then(response => {
       return response || fetch(event.request);
