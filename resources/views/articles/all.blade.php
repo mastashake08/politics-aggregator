c2220285540d88f7ed3a59c3fff1e75898260816
Jyrone Parker <jyrone.parker@gmail.com>
2019-03-05 23:10:23 -0500

    remove ads

--- resources/views/articles/all.blade.php
+++ resources/views/articles/all.blade.php
@@ -2,16 +2,6 @@
 
 @section('content')
 <div class="container">
-  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-  <!-- all-around-politics -->
-  <ins class="adsbygoogle"
-       style="display:block"
-       data-ad-client="ca-pub-7023023584987784"
-       data-ad-slot="2351142156"
-       data-ad-format="auto"></ins>
-  <script>
-  (adsbygoogle = window.adsbygoogle || []).push({});
-  </script>
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
