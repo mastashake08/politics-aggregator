a6a1d8a401b3f100a92a47cfa12d4cedbfbf4211
Jyrone Parker <jyrone.parker@gmail.com>
2019-03-05 23:41:32 -0500

    "

--- public/js/sw.js
+++ public/js/sw.js
@@ -4,14 +4,14 @@ var filesToCache = [
   '/css/app.css',
   '/js/app.js',
   '/',
-  "/images/icons/icon-72x72.png",
-  "/images/icons/icon-96x96.png",
-  "/images/icons/icon-128x128.png",
-  "/images/icons/icon-144x144.png",
-  "/images/icons/icon-152x152.png",
-  "/images/icons/icon-192x192.png",
-  "/images/icons/icon-384x384.png",
-  "/images/icons/icon-512x512.png"
+  '/images/icons/icon-72x72.png',
+  '/images/icons/icon-96x96.png',
+  '/images/icons/icon-128x128.png',
+  '/images/icons/icon-144x144.png',
+  '/images/icons/icon-152x152.png',
+  '/images/icons/icon-192x192.png',
+  '/images/icons/icon-384x384.png',
+  '/images/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', function(e) {
@@ -27,7 +27,7 @@ self.addEventListener('activate',  event => {
   event.waitUntil(self.clients.claim());
 });
 self.addEventListener('fetch', event => {
-  console.log("Fetch",event)
+  console.log('Fetch',event)
   event.respondWith(
     caches.match(event.request, {ignoreSearch:true}).then(response => {
       return response || fetch(event.request);
